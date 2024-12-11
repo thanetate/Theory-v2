9416837d9161c31f97f152fd1259fc7e69ad4f42
Tate, Thane <thanetate@my.unt.edu>
2024-12-11 17:14:28 -0600

    promoBar feature

---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,8 @@
+import { PromoBar } from "../PromoBar/PromoBar";
+export function Header() {
+    return (
+            <div className="header">
+                <PromoBar />
+            </div>
+    );
+}
---
+++ client/src/components/PromoBar/PromoBar.tsx
@@ -0,0 +1,10 @@
+import "./PromoBar.css";
+export function PromoBar() {
+	return (
+		<div className="promo-bar">
+			<div className="marquee-container">
+				<div className="marquee-content">Free shipping on orders over $100</div>
+			</div>
+		</div>
+	);
+}
