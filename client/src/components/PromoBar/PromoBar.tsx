9174ab8177c59074b03587eed79f6be9d8fe5eaf
Tate, Thane <thanetate@my.unt.edu>
2024-12-13 20:30:08 -0600

    marquee

--- client/src/components/PromoBar/PromoBar.tsx
+++ client/src/components/PromoBar/PromoBar.tsx
@@ -1,10 +1,12 @@
+import Marquee from "react-fast-marquee";
 import "./PromoBar.css";
+
 export function PromoBar() {
 	return (
 		<div className="promo-bar">
-			<div className="marquee-container">
-				<div className="marquee-content">Free shipping on orders over $100</div>
-			</div>
+			<Marquee>
+				  <span className="marquee-text">Free shipping on orders over $100</span>
+			</Marquee>
 		</div>
 	);
 }
