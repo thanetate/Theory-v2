c03b4209ab19b2850c9933a3be09ef779f694d70
Tate, Thane <thanetate@my.unt.edu>
2024-12-15 11:30:14 -0600

    Carousel working

--- client/src/components/Carousel/Carousel.tsx
+++ client/src/components/Carousel/Carousel.tsx
@@ -1,30 +1,50 @@
 import "./Carousel.css";
+import { useState } from "react";
+
+const images = [
+	"./Carousel2.png",
+	"./Carousel6.png",
+	"./Carousel4.png",
+	"./Carousel3.png",
+	"./Carousel1.png",
+	"./Carousel5.png",
+];
 
 export function Carousel() {
+	const [currentImage, setCurrentImage] = useState(0);
+
+	const handlePrevClick = () => {
+		setCurrentImage((prevIndex) =>
+			prevIndex === 0 ? images.length - 1 : prevIndex - 1
+		);
+	};
+	const handleNextClick = () => {
+		setCurrentImage((prevIndex) =>
+			prevIndex === images.length - 1 ? 0 : prevIndex + 1
+		);
+	};
+
 	return (
 		<>
 			<div className="carousel-container">
-				<div className="carousel-img"></div>
+				<div className="carousel-img">
+					<img src={images[currentImage]} alt="Carousel" />
+				</div>
 				<div className="carousel-btn-container">
-					<button className="carousel-btn">
+					<button className="carousel-btn" onClick={handlePrevClick}>
 						<img src="./icons/leftarrow.svg" alt="Left Arrow" />
 					</button>
-					<div className="carousel-circle">
-						<img src="./icons/circle.svg" alt="Circle" />
-					</div>
-					<div className="carousel-circle">
-						<img src="./icons/circle.svg" alt="Circle" />
-					</div>
-					<div className="carousel-circle">
-						<img src="./icons/circle.svg" alt="Circle" />
-					</div>
-					<div className="carousel-circle">
-						<img src="./icons/circle.svg" alt="Circle" />
-					</div>
-					<div className="carousel-circle">
-						<img src="./icons/circle.svg" alt="Circle" />
-					</div>
-					<button className="carousel-btn">
+					{images.map((_, index) => (
+						<div
+							key={index}
+							className={`carousel-circle ${
+								index === currentImage ? "active" : ""
+							}`}
+						>
+							<img src="./icons/circle.svg" alt="Circle" />
+						</div>
+					))}
+					<button className="carousel-btn" onClick={handleNextClick}>
 						<img src="./icons/rightarrow.svg" alt="Right Arrow" />
 					</button>
 				</div>
