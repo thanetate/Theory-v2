a95bb549fc05a4827bec00c8c0a0756cd9a2767a
Tate, Thane <thanetate@my.unt.edu>
2024-12-17 12:45:24 -0600

    about responsive

--- client/src/components/About/About.tsx
+++ client/src/components/About/About.tsx
@@ -6,12 +6,7 @@ export function About() {
 			<div className="about-container">
 				<h1 className="about-logo">Theory</h1>
 				<h2 className="about-desc">
-					Theory is a climbing brand born from a passion for minimalism and
-					design. With its focus on functional elegance, the brand draws
-					inspiration from the climbing community, minimalist aesthetics, and
-					the unspoken connection between climbers. Theory isn’t just
-					clothing—it’s the perfect balance of practicality and design, made for
-					climbers.
+					"Functional, Minimal, Modern."
 				</h2>
 			</div>
 		</>
