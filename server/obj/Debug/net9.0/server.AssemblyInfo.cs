80392f532226cc1ae6913c04a7992807e53b2bed
Tate, Thane <thanetate@my.unt.edu>
2024-12-22 15:25:24 -0600

    rm supabase-csharp

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af14905841352e41dff121b5ec41397bef6bb794")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fec93b3898eb56d34a7ba0b456ab703ecd309c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
