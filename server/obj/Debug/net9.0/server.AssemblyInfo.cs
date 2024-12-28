2dd8c98af620c60785ca7cadf96d1ffe6695c19d
Tate, Thane <thanetate@my.unt.edu>
2024-12-28 13:43:00 -0600

    auth working

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00f26481a47b374709f1f19ebc78b77274eea3d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38aa8be664830fa240b1baff083e7881d254289c")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
