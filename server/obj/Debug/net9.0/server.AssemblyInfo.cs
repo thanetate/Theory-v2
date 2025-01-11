83c5d14930f14ef769644b68d97ac12bc63f9e56
Tate, Thane <thanetate@my.unt.edu>
2025-01-11 10:54:45 -0600

    git ignore

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b9b7564b0fe08911f4d5d740d156fd27b17696c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+725153458e98dfc702bdf5ef89ba7c458de20822")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
