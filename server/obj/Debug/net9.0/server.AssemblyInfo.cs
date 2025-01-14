4863534a5f0e867db4c76212068fff3f6e5f6e95
Tate, Thane <thanetate@my.unt.edu>
2025-01-13 19:11:06 -0600

    deploy to azure

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbf296009393a5165b7eeca493a20cc3b3620c5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+398ae3d85c211803b6d10266efe20db572698b37")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
