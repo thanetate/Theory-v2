ed1490861076a35ae0b45d24b53ecd8fda4f7124
Tate, Thane <thanetate@my.unt.edu>
2025-01-15 14:35:15 -0600

    routes vercel

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df7f416843830e23643a1b5ad094a11b6631b16c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04553ab6fde558dc7328c9144449b10d3848749f")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
