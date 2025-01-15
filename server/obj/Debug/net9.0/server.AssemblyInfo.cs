df7f416843830e23643a1b5ad094a11b6631b16c
Tate, Thane <thanetate@my.unt.edu>
2025-01-15 14:27:36 -0600

    build -> dist

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6c67b7b9ceae24e53b1b42f2bc3da60e6428f5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66c21979bb6856c687f768cd95ec0e6c7ec22b5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
