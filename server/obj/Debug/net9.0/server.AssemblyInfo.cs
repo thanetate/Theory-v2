e4c29b4395818fb04391606f5d5540caa246478c
Tate, Thane <thanetate@my.unt.edu>
2025-01-17 21:28:02 -0600

    changed logo

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a33b96e2fab324a0e6a2ee07145a8c7ed40e58d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb0fa831ff2543e31371e9a9d5a12aa076e44a52")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
