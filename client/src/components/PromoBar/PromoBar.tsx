5b6a2412e7f39f8b9a0bbd6d258af3379e13a899
Tate, Thane <thanetate@my.unt.edu>
2025-01-21 20:46:05 -0600

    update promo bar message to indicate site under construction

--- client/src/components/PromoBar/PromoBar.tsx
+++ client/src/components/PromoBar/PromoBar.tsx
@@ -7,14 +7,14 @@ export function PromoBar() {
 		<div className="promo-bar">
 		<div style={{ backgroundColor: "#1C1C1C" , padding: "5px 0"}}>
 			<Marquee>
-				  <span className="marquee-text">Free shipping on all orders</span>
-				  <span className="marquee-text">Free shipping on all orders</span>
-				  <span className="marquee-text">Free shipping on all orders</span>
-				  <span className="marquee-text">Free shipping on all orders</span>
-				  <span className="marquee-text">Free shipping on all orders</span>
-				  <span className="marquee-text">Free shipping on all orders</span>
-				  <span className="marquee-text">Free shipping on all orders</span>
-				  <span className="marquee-text">Free shipping on all orders</span>
+				  <span className="marquee-text">🚨 This site is currently under construction</span>
+				  <span className="marquee-text">🚨 This site is currently under construction</span>
+				  <span className="marquee-text">🚨 This site is currently under construction</span>
+				  <span className="marquee-text">🚨 This site is currently under construction</span>
+				  <span className="marquee-text">🚨 This site is currently under construction</span>
+				  <span className="marquee-text">🚨 This site is currently under construction</span>
+				  <span className="marquee-text">🚨 This site is currently under construction</span>
+				  <span className="marquee-text">🚨 This site is currently under construction</span>
 			</Marquee>
 			</div>
 		</div>
--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdf25be92f664b894351896ce676b418d0be5135")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abe64c9c1dddee4b1910963fdd6e11baf5908a38")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
