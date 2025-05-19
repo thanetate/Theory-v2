d449db3952211c104c1722f6618630478f425404
Tate, Thane <thanetate@my.unt.edu>
2025-05-19 13:23:44 -0500

    main working

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9bf33abdaeefced59b4eed983bd5daca9765425")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89fa8ad69abdecb9229a2595b1d44fd191488770")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
