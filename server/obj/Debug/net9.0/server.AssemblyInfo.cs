2f707b86b9cf7ea11c49d3c73b49c6268dadb9ef
Tate, Thane <thanetate@my.unt.edu>
2025-05-20 12:46:01 -0500

    env variables

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89ff7e73b24de4abf9bbc0a301e09dd3b5d06440")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5a9c94bcf0d51eff0055dc9f1e12d5c63d64331")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
